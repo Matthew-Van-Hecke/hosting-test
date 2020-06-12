1ff29fc5c4a447907582217c8b90902a0bf82905
Matthew VH <vanheckematt@gmail.com>
2020-06-12 11:25:36 -0500

    Updates

---
+++ precache-manifest.8aa08ee01c21f14d5e670e54d88599d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f2ec16649ae296f7b0d0fd9c6d2ef9",
+    "url": "/hosting-test/index.html"
+  },
+  {
+    "revision": "ed8fe8ad955dc4c79908",
+    "url": "/hosting-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98250360ffead32790f0",
+    "url": "/hosting-test/static/js/2.662e71fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hosting-test/static/js/2.662e71fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8fe8ad955dc4c79908",
+    "url": "/hosting-test/static/js/main.a903c58d.chunk.js"
+  },
+  {
+    "revision": "9906be3045994664f903",
+    "url": "/hosting-test/static/js/runtime-main.751f6ec4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hosting-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
